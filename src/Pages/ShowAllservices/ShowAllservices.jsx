73c3699da71b1168afa6429617a5b04422da8c5e
Mahadi1000 <muhammadmahdihasan1@gmail.com>
2023-11-09 20:53:12 +0600

    dymiacally details route for a service

--- src/Pages/ShowAllservices/ShowAllservices.jsx
+++ src/Pages/ShowAllservices/ShowAllservices.jsx
@@ -21,13 +21,37 @@ function ShowAllService() {
 
   return (
     <div className="mt-28">
-      <h1>Show All Service Details</h1>
-      {typeData.map((service) => (
-        <div key={service.id}>
-          <h2>Service Name: {service.name}</h2>
-          {/* Render other service details here */}
-        </div>
-      ))}
+      <h1 className="text-center text-4xl font-bold">
+        Available Service Details
+      </h1>
+      <div className="grid grid-cols-2 max-w-6xl mx-auto gap-10 mt-10">
+        {typeData.map((service) => (
+          <div
+            key={service.id}
+            className="bg-transparent  p-4 rounded-lg shadow-lg transition-transform duration-500 transform hover:scale-105"
+          >
+            <div>
+              <img className="h-96 w-full" src={service.photo} alt="" />
+            </div>
+            <div className="flex gap-4 items-center mt-3 ">
+              <div className="avatar">
+                <div className="w-16 rounded-full">
+                  <img src={service.providerImage} />
+                </div>
+              </div>
+              <div>
+                <h1 className="text-xl  text-center font-semibold p-4">
+                  {service.name}
+                </h1>
+              </div>
+            </div>
+            <p className="px-5 py-3">{service.description}</p>
+            <div className="flex justify-center ">
+              <button className="btn btn-outline btn-success">View Details</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
